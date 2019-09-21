61fbb47d1f3dbbc2f59f19840aeec9ca136a73ac
benntoboxbenjaminsboyle3@gmail.com <benjaminsboyle3@gmail.com>
2019-09-20 22:46:14 -0700

    Army, Detachment and Unit started

--- ArmyBuilder/main.cpp
+++ ArmyBuilder/main.cpp
@@ -26,7 +26,12 @@ public:
 };
 
 class Unit {
-    Model models;
+    vector<Model> unit;
+    string description;
+public:
+    Unit() { description = "";}
+    Unit(string des) { description = des; }
+    string print_unit() { return description; }
 };
 
 class HQ : Unit {
@@ -55,15 +60,34 @@ class HeavySupport : Unit {
 };
 
 class Detachment {
-    vector<HQ> HeadQuarters;
-    Unit unit1;
+    vector<Unit> units;
+public:
+    Detachment() { units = {}; }
+    void add_unit(Unit unit) { units.push_back(unit); }
+    void print_units() { for(int i=0; i<units.size(); i++) { cout << units[i].print_unit() << endl; } }
 };
 
 class Army {
     Detachment det1;
+public:
+    Army() { det1 = * new Detachment; }
+    void add_unit(Unit unit) { det1.add_unit(unit); }
+    void print_units() { cout << "Army consists of :" << endl; det1.print_units(); }
 };
 
 int main(int argc, const char * argv[]) {
+    Army army = * new Army;
+    
+    Unit unit1 = Unit("Marine");
+    Unit unit2 = Unit("Captain");
+    Unit unit3 = Unit("Commander");
+    
+    army.print_units();
+    army.add_unit(unit1);
+    army.print_units();
+    army.add_unit(unit2);
+    army.add_unit(unit3);
+    army.print_units();
     
     return 0;
 }
